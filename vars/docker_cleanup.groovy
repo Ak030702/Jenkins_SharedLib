ecacc70b8edda4828c9d5dfdc699fe8491457508
Abhishek Kemana <93156114+Ak030702@users.noreply.github.com>
2025-11-12 02:58:59 +0530

    Use sudo for docker rmi command

--- vars/docker_cleanup.groovy
+++ vars/docker_cleanup.groovy
@@ -1,3 +1,3 @@
 def call(String Project, String ImageTag, String DockerHubUser){
-  sh "docker rmi ${DockerHubUser}/${Project}:${ImageTag}"
+  sh "sudo docker rmi ${DockerHubUser}/${Project}:${ImageTag}"
 }
